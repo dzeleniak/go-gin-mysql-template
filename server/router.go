8b7b82014b911ca53d66ecaae7ff6cf744aaa3a6
dzeleniak <zeleniak.david@gmail.com>
2023-03-20 11:20:21 -0700

    Object Controller Setup

--- server/router.go
+++ server/router.go
@@ -1,21 +1,24 @@
 package server
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/dzeleniak/go-gin-mysql-template/controllers"
+	"github.com/gin-gonic/gin"
+)
 
 func NewRouter() *gin.Engine {
 	router := gin.New()
 	router.Use(gin.Logger())
 	router.Use(gin.Recovery())
 
-	// objectGroup := router.Group("object")
-	// {
-	// 	object := new(controllers.ObjectController)
-	// 	objectGroup.POST("/", object.Create)
-	// 	objectGroup.GET("/", object.All)
-	// 	objectGroup.GET("/:id", object.One)
-	// 	objectGroup.DELETE("/:id", object.Delete)
-	// 	objectGroup.PUT("/:id", object.Update)
-	// }
+	objectGroup := router.Group("object")
+	{
+		object := new(controllers.ObjectController)
+		objectGroup.POST("/", object.Create)
+		objectGroup.GET("/", object.All)
+		objectGroup.GET("/:id", object.One)
+		objectGroup.DELETE("/:id", object.Delete)
+		objectGroup.PUT("/:id", object.Update)
+	}
 
 	// router.Use(middlewares.AuthMiddleware())
 
